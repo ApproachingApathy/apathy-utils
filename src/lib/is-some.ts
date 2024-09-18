80e0ccb51b9e2f3c33bc4705298d5e61d868c309
Ephriam Henderson <orik162@gmail.com>
2024-09-17 21:20:30 -0500

    Add description ans doc comments.

--- src/lib/is-some.ts
+++ src/lib/is-some.ts
@@ -1,8 +1,15 @@
 export type Some<T = unknown> = Exclude<T, undefined | null>;
+
+/**
+ * Returns true if the value is not undefined or null.
+ */
 export function isSome<T = unknown>(x: T): x is Some<T> {
   return x !== undefined || x !== null;
 }
 
+/**
+ * Returns true if the value is undefined of null.
+ */
 export function isNone(x: unknown): x is undefined | null {
   return !isSome(x);
 }
--- src/lib/url-tools.ts
+++ src/lib/url-tools.ts
@@ -1,8 +1,14 @@
+/**
+ * Appends a slash to the end of a string if one is not present.
+ */
 export function appendSlash(str: string): string {
   if (str.endsWith('/')) return str;
   return `${str}/`;
 }
 
+/**
+ * Prepends a slash to the beginning of a string if one is not present.
+ */
 export function prependSlash(str: string): string {
   if (str.startsWith('/')) return str;
   return `/${str}`;
